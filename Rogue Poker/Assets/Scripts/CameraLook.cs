4d9da65ef2094aaa8cdaee54348355c411d0516e
Connor Boscherini <connorbos@hotmail.co.uk>
2021-11-27 17:59:00 +0000

    UI Set-up
    Set up environment for UI development. Added basic structure into new scene. Also organised folder structure for future progress

---
+++ Rogue Poker/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        // Rotate the camera every frame so it keeps looking at the target
+        transform.LookAt(target);
+    }
+}
