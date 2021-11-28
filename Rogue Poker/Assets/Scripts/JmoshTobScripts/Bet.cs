4a2702bcdac73e1c66570da7c357096e126506b9
jkinver <joshua.kinver@students.plymouth.ac.uk>
2021-11-28 13:56:49 +0000

    Added A Method
    Added ConfirmBets method and some variables for the PlayerBets.

--- Rogue Poker/Assets/Scripts/JmoshTobScripts/Bet.cs
+++ Rogue Poker/Assets/Scripts/JmoshTobScripts/Bet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bet : MonoBehaviour
 {
+    [SerializeField]
+    private int PlayerRockBet, PlayerScissorsBet, PlayerPaperBet;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,14 @@ public class Bet : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void ConfirmBets(string type)
+    {
+        switch (type)
+        {
+            case "Rock":
+                ;
+        }
     }
 }
