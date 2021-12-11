42e19a6098e1ed9b7f58e76155296d5444e9f77b
Connor Boscherini <connorbos@hotmail.co.uk>
2021-12-11 01:56:56 +0000

    Ai setup
    Finished base opponent functionality. completed bet options and added logic for raising. Still needing to add Call system, but required checking Phase script first

--- Rogue Poker/Assets/Scripts/NewScripts/NAI.cs
+++ Rogue Poker/Assets/Scripts/NewScripts/NAI.cs
@@ -17,29 +17,29 @@ public class NAI : MonoBehaviour
         switch (option)
         {
             case 1:
-                nums = new int[] { 0, 2, 0, 20, 0, 0 };
+                nums = new int[] { 0, 3, 0, 20, 0, 0 };
                 opponentBets.AlterBet(0, nums);
-                nums = new int[] { 0, 0, 0, 17, 0, 0 };
+                nums = new int[] { 0, 1, 5, 0, 4, 0 };
                 opponentBets.AlterBet(1, nums);
-                nums = new int[] { 0, 5, 0, 0, 0, 0 };
+                nums = new int[] { 2, 0, 0, 0, 0, 12 };
                 opponentBets.AlterBet(2, nums);
                 break;
 
             case 2:
-                nums = new int[] { 0, 2, 0, 20, 0, 0 };
+                nums = new int[] { 2, 0, 1, 0, 0, 0 };
                 opponentBets.AlterBet(0, nums);
-                nums = new int[] { 0, 0, 0, 17, 0, 0 };
+                nums = new int[] { 0, 2, 2, 7, 1, 15 };
                 opponentBets.AlterBet(1, nums);
-                nums = new int[] { 0, 5, 0, 0, 0, 0 };
+                nums = new int[] { 0, 2, 5, 0, 10, 0 };
                 opponentBets.AlterBet(2, nums);
                 break;
 
             case 3:
-                nums = new int[] { 0, 2, 0, 20, 0, 0 };
+                nums = new int[] { 0, 1, 0, 4, 0, 0 };
                 opponentBets.AlterBet(0, nums);
-                nums = new int[] { 0, 0, 0, 17, 0, 0 };
+                nums = new int[] { 0, 2, 3, 0, 0, 0 };
                 opponentBets.AlterBet(1, nums);
-                nums = new int[] { 0, 5, 0, 0, 0, 0 };
+                nums = new int[] { 5, 0, 0, 0, 0, 0 };
                 opponentBets.AlterBet(2, nums);
                 break;
         }
@@ -47,26 +47,124 @@ public class NAI : MonoBehaviour
 
     public void RaiseFold()
     {
-        System.Random generate = new System.Random();
-        int option = generate.Next(0, 4);
-        int[] nums;
-        switch (option)
+        int[] bets = opponentBets.returnAllValues();
+        int lowest = 2, highest = 0, lowestval = 49, highestVal = 501;
+        //identify highest bet and lowest bet
+        for (int i = 0; i < bets.Length; i++)
         {
-            case 1:
-                nums = new int[] { 0, 2, 0, 20, 0, 0 };
+            if (bets[i] > lowestval)
+            {
+                highest = i;
+            }
+            if (bets[i] < highestVal)
+            {
+                lowest = i;
+            }
+        }
+
+        //Raising
+        int[] nums = { 0, 0, 0, 0, 0, 0 };
+
+        int dif = 0;
+        double temp = highestVal / 2;
+        //some pre-set partial raises depending on values. All leave some spare change
+        if (highestVal > 450)
+        {
+            nums[0] = 2;
+            nums[2] = 1;
+            dif = (int)Math.Round(temp) - 220;
+        }
+        else if (highestVal > 400)
+        {
+            nums[0] = 2;
+            dif = (int)Math.Round(temp) - 200;
+        }
+        else if (highestVal > 350)
+        {
+            nums[1] = 3;
+            nums[2] = 1;
+            dif = (int)Math.Round(temp) - 170;
+        }
+        else if (highestVal > 300)
+        {
+            nums[2] = 7;
+            dif = (int)Math.Round(temp) - 140;
+        }
+        else if (highestVal > 250)
+        {
+            nums[0] = 1;
+            nums[4] = 4;
+            dif = (int)Math.Round(temp) - 120;
+        }
+        else if (highestVal > 200)
+        {
+            nums[3] = 9;
+            dif = (int)Math.Round(temp) - 90;
+        }
+        else if (highestVal > 150)
+        {
+            nums[2] = 3;
+            nums[4] = 2;
+            dif = (int)Math.Round(temp) - 70;
+        }
+        else if (highestVal > 100)
+        {
+            nums[3] = 4;
+            dif = (int)Math.Round(temp) - 40;
+        }
+        else if (highestVal > 50)
+        {
+            dif = (int)Math.Round(temp);
+        }
+
+        while (dif > 0)
+        {
+            //keep adding to raise until difference is made up
+            if (dif - 50 >= 0)
+            {
+                nums[1] += 1;
+                dif -= 50;
+            }
+            else if (dif - 20 >= 0)
+            {
+                nums[2] += 1;
+                dif -= 20;
+            }
+            else if (dif - 10 >= 0)
+            {
+                nums[3] += 1;
+                dif -= 10;
+            }
+            else if (dif - 5 >= 0)
+            {
+                nums[4] += 1;
+                dif -= 5;
+            }
+            else if (dif - 1 >= 0)
+            {
+                nums[5] += 1;
+                dif -= 1;
+            }
+        }
+
+        //Identify which is highest and apply calculated Raise
+        switch (highest)
+        {//Somewhere here, need to notify UI and allow player to Call
+            case 0:
                 opponentBets.AlterBet(0, nums);
                 break;
 
-            case 2:
-                nums = new int[] { 0, 0, 0, 17, 0, 0 };
+            case 1:
                 opponentBets.AlterBet(1, nums);
                 break;
 
-            case 3:
-                nums = new int[] { 0, 5, 0, 0, 0, 0 };
+            case 2:
                 opponentBets.AlterBet(2, nums);
                 break;
         }
+
+        //Fold lowest valued bet
+        opponentBets.setFoldIndex(lowest);
     }
 
     public void Call()
