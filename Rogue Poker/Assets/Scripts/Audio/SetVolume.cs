b7fbb8cf0b4b4902eb6dfe10374514fa8d375708
SchaefferGeorgiou <55706739+SchaefferGeorgiou@users.noreply.github.com>
2021-12-11 16:24:39 +0000

    Created new branch and scene. Added initial audio files
    Created AudioScene to import and test imported AudioManager from Losing Track.
    
    Created subfolders for audio scripts and audio objects.

---
+++ Rogue Poker/Assets/Scripts/Audio/SetVolume.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    //Audio Mixer used in this Scene
+    public AudioMixer mixer;
+
+
+    //Sets the Volume of everything in the game
+    public void SetMasterVol(float sliderValue)
+    {
+        mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+        Debug.Log("Master Volume Set to " + Mathf.Log10(sliderValue) * 20);
+    }
+
+    //Sets the Music Volume
+    public void SetMusicVol(float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+    //Sets the Sound Effects Volume
+    public void SetFXVol(float sliderValue)
+    {
+        mixer.SetFloat("FXVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+}
---
+++ Rogue Poker/Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Sound 
+{
+    public string name;
+
+    public AudioClip clip;
+
+    public AudioMixerGroup mix;
+
+    [Range (0f, 1f)]
+    public float volume;
+
+    [Range(0f, 3f)]
+    public float pitch;
+
+    [Range (-1f, 1f)]
+    public float pan;
+
+    public bool onAwake;
+    public bool loop;
+    public bool mute;    
+
+    [HideInInspector]
+    public AudioSource source;
+
+
+
+    public void setPan(float newPan)
+    {
+        pan = newPan;
+    }
+
+    public void setPitch(float newPitch)
+    {
+        pitch = newPitch;
+    }
+
+    public void setVolume(float newVolume)
+    {
+        volume = newVolume;
+    }
+}
