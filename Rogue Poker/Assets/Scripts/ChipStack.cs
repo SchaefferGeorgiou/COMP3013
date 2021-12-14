b1b5926c2a585b766fe106dcf74e6992d1f5cc0f
Connor Boscherini <connorbos@hotmail.co.uk>
2021-12-14 22:07:29 +0000

    Chip stack + folder management
    Minor edits to the chip stack script and some clean up with the folder structure in Assets

--- Rogue Poker/Assets/Scripts/ChipStack.cs
+++ Rogue Poker/Assets/Scripts/ChipStack.cs
@@ -7,8 +7,8 @@ public class ChipStack : MonoBehaviour
     //note: this script is to be put onto empty GameObjects that serve as the base for each individual stack
 
     //declaring variables
-    private float chipHeight; //a number for the height of a single chip
-    private float currentHeight; //a number for the height of the current stack; it is to be incremented upon adding chips
+    [SerializeField, Header("Gap size")]
+    private float GapSize = 0.02f; //Size of the gap between each chip
     [SerializeField]
     GameObject chip; //a gameObject for the reference chip to use for the particular stack
     Vector3 stackPosition; //the chip stack's position
@@ -17,16 +17,14 @@ public class ChipStack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        chipHeight = chip.GetComponent<Renderer>().bounds.size.y + 0.02f; 
+        GapSize += chip.GetComponent<Renderer>().bounds.size.y; 
         stackPosition = transform.position;
         Debug.Log(transform.position.y);
-
-        currentHeight = stackPosition.y;
 
         //for loop for the sake of testing without a button
         for (int i = 0; i < 5; i++)
         {
-            StackChips();
+            StackChips(1);
         }
 
         StartCoroutine(Wait());
@@ -39,19 +37,21 @@ public class ChipStack : MonoBehaviour
     }
 
     //a method for stacking individual chips as they are added (progressively) in the Betting Panel
-    void StackChips()
+    void StackChips(int numChips)
     {
-        //create a chip
-        GameObject temp = Instantiate(chip, stackPosition, chip.transform.rotation * Quaternion.Euler(0f, 0f, 90f));
-        chipStack.Add(temp); 
-        currentHeight += chipHeight; //the current height changes by a chip height increment
-        stackPosition.y = currentHeight; //the y value of the stack's position changes by the height
+        for (int i = 0; i < numChips; i++)
+        {
+            //create a chip, save it and then add it to the stack
+            GameObject temp = Instantiate(chip, stackPosition, chip.transform.rotation * Quaternion.Euler(0f, 0f, 90f));
+            chipStack.Add(temp);
+            stackPosition.y += GapSize; //the y value of the stack's position changes by the height
+        }
     }
 
     //a method for removing individual chips as they are subtracted (progressively) in the Betting Panel
-    void RemoveChips(int numLostChips)
+    void RemoveChips(int numChips)
     {
-        for (int i = 0; i < numLostChips; i++)
+        for (int i = 0; i < numChips; i++)
         {
             //destroys last chip element
             Destroy(chipStack[chipStack.Count - 1]);
