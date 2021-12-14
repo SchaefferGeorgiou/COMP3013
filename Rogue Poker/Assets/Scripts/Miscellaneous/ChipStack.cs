4063e1984b48ddd30e53068b15786cf5b92c4c30
jkinver <joshua.kinver@students.plymouth.ac.uk>
2021-12-14 17:46:38 +0000

    Started ChipStack
    The ChipStack script for stacking the chips placed on each bet has been started. It currently contains a float value for the height at which the next chip should be placed and an array of gameObjects to act as the 'base' of each chip.
    Next, each gameObject for the chips themselves need to be added as well as utilising the position of each object in the array.

---
+++ Rogue Poker/Assets/Scripts/Miscellaneous/ChipStack.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChipStack : MonoBehaviour
+{
+    //declaring variables
+    [SerializeField]
+    float chipHeight; //a number for the height at which the next chip shall be stacked  
+    [SerializeField]
+    GameObject[] stackBases; //an array of gameObjects that are to be empty and used to position each chip stack
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
